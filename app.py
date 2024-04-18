f8586d4ec211b05ceb50d641e19d2b482ac791c5
Gokul Palanisamy <gokulp@bu.edu>
2024-04-18 16:22:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Test import get_few_shot_db_chain
+
 
 st.image('/Users/gokul/Desktop/Believe/Projects/langchain/Main/images/Staples_logo_PNG5.png', caption='Business is  Human')
 
