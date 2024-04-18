5526d65794f76b37c6216f2fd0731716a380e8f2
Gokul Palanisamy <gokulp@bu.edu>
2024-04-18 16:14:54 -0400

    Update app.py
    commiting new change

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+from Test import get_few_shot_db_chain
+
+st.image('/Users/gokul/Desktop/Believe/Projects/langchain/Main/images/Staples_logo_PNG5.png', caption='Business is  Human')
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_few_shot_db_chain()
+    response = chain.run(question)
+
+    st.header("Answer")
+    st.write(response)
+
+
+
+
+
 
