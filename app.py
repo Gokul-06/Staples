0cc34fc3827b63f81cd120b934584509850c5bb4
Gokul Palanisamy <gokulp@bu.edu>
2024-04-18 16:22:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.image('/Users/gokul/Desktop/Believe/Projects/langchain/Main/images/Staples_logo_PNG5.png', caption='Business is  Human')
+
 
 question = st.text_input("Question: ")
 
